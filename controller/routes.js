2fad17f50cada903baecf3006d2e824890e3b319
Shivakanya <shivakanya@Shivakanyas-MacBook-Air.local>
2022-07-06 17:36:29 +0530

    task

--- controller/routes.js
+++ controller/routes.js
@@ -20,18 +20,54 @@ const getAllRoutes = async () => {
     data?.map((item) => {
       routedetail.push(moment(item.created_at).format("MM"))
     })
-    console.log(routedetail)
-    const count = {}
-    for (const element of routedetail) {
-      if (count[element]) {
-        count[element] += 1
-      } else {
-        count[element] = 1
-      }
-    }
+    // console.log(routedetail)
+    // const count = {}
+    // for (const element of routedetail) {
+    //   if (count[element]) {
+    //     count[element] += 1
+    //   } else {
+    //     count[element] = 1
+    //   }
+    // }
 
-    console.log(count, "cc")
-    return count
+    // console.log(count, "cc")
+    // return count
+
+    const obj = {
+        "01": 0,
+        "02": 0,
+        "03": 0,
+        "04": 0,
+        "05": 0,
+        "06": 0,
+        "07": 0,
+        "08": 0,
+        "09": 0,
+        10: 0,
+        11: 0,
+        12: 0
+      }
+  
+      routedetail?.map((item) => {
+        obj[item] = obj[item] + 1
+      })
+      
+  
+      let index = Object.keys(obj).sort()
+  
+      // iterate method
+      let details = []
+      index.forEach((key) => {
+        console.log(key, obj[key])
+        details.push(obj[key])
+      })
+  
+      // or array of sorted object
+      let sortedArrayOfObject = index.map((v) => {
+        return { key: v, value: obj[v] }
+      })
+      
+      return details
   } catch (error) {
     console.log(error)
     return {
